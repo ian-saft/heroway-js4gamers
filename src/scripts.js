66d5bcef43f75989247332bf2b723b822d590034
Ian Saft <devdoamanha@gmail.com>
2021-02-27 06:01:59 -0300

    game-v1.1

--- src/scripts.js
+++ src/scripts.js
@@ -12,9 +12,13 @@ root.style.setProperty('--game-size', `${GAME_SIZE}px`);
 function createBoard() {
 
     const boardElement = document.getElementById('board')
+    const elements = [];
 
     function createElement(options) {
         let { item , top , left} = options;
+
+        const currentElement = { item , currentPosition: { top , left }};
+        elements.push(currentElement);
 
         const htmlElement = document.createElement('div');
         htmlElement.className  = item;
@@ -22,30 +26,77 @@ function createBoard() {
         htmlElement.style.left = `${left}px`
 
         boardElement.appendChild(htmlElement)
-
-        function getNewDirection(buttonPressed) {
+        
+        function getNewDirection(buttonPressed, position) {
             switch (buttonPressed) {
                 case 'ArrowUp':
-                    return { top: top - TILE_SIZE , left: left };
+                    return { top: (position.top - TILE_SIZE) , left: position.left };
                 case 'ArrowRight':
-                    return { top: top , left: left + TILE_SIZE };
+                    return { top: position.top , left: (position.left + TILE_SIZE) };
                 case 'ArrowDown':
-                    return { top: top + TILE_SIZE , left: left };
+                    return { top: (position.top + TILE_SIZE) , left: position.left };
                 case 'ArrowLeft':
-                    return { top: top , left: left - TILE_SIZE };
+                    return { top: position.top , left: (position.left - TILE_SIZE) };
                 default:
-                    return { top: top , left: left};
+                    return { top: position.top , left: position.left};
             }
         }
-    
+
+        function getElementConflict(position, els) {
+            const conflictItem = els.find((el) => {
+                return (
+                    el.currentPosition.top === position.top &&
+                    el.currentPosition.left === position.left
+                )
+            });
+
+            return conflictItem;
+        }
+
+        function validateConflicts(currentEl, conflictItem) {
+
+            function finishGame(message) {
+                alert(message);
+                location.reload();
+            }
+
+            if (currentEl.item === 'hero'){
+                if (conflictItem?.item === 'mini-demon' || conflictItem?.item === 'trap') {
+                    finishGame('YOU DIE!');
+                }
+                if (conflictItem?.item === 'chest') {
+                    finishGame('YOU WIN!');
+                }
+            }
+            if (currentEl.item === 'mini-demon' && conflictItem?.item === 'hero') {
+                finishGame('YOU DIE!');
+            }
+        }
+        
+        function validateMovimente(position, conflictItem) {
+            
+            return (
+                position.left >= 24 &&
+                position.left <= 864 &&
+                position.top >= 96 &&
+                position.top <= 816 &&
+                conflictItem?.item !== 'forniture'
+            )
+        }
+
         function move(buttonPressed) {
     
-            const newDirection = getNewDirection(buttonPressed);
-            htmlElement.style.top  = `${newDirection.top}px`;
-            htmlElement.style.left = `${newDirection.left}px`;
-    
-            top  = newDirection.top;
-            left = newDirection.left;
+            const newPosition = getNewDirection(buttonPressed, currentElement.currentPosition);
+            const conflictItem = getElementConflict(newPosition, elements);
+            const isValidMoviment = validateMovimente(newPosition, conflictItem);
+
+            if (isValidMoviment) {
+                currentElement.currentPosition.top  = newPosition.top;
+                currentElement.currentPosition.left = newPosition.left;
+                htmlElement.style.top  = `${newPosition.top}px`;
+                htmlElement.style.left = `${newPosition.left}px`;
+                validateConflicts(currentElement, conflictItem);
+            }
         }
 
         return { move };
@@ -79,23 +130,93 @@ function createBoard() {
         }, 1000)
     }
 
-    function createTrap(options) {
-        createElement({
-            item: 'trap',
-            top: options.top,
-            left: options.left
-        })
-    }
-
     return { 
+        createElement: createElement,
         createHero: createHero,
         createDemon: createDemon,
-        createTrap: createTrap
     }
 }
 
 const board = createBoard();
 
-board.createHero({ top: 16 * TILE_SIZE, left: 2 * TILE_SIZE});
-board.createDemon({ top: 16 * TILE_SIZE, left: 7 * TILE_SIZE});
-board.createTrap({ top: 16 * TILE_SIZE, left: 8 * TILE_SIZE});
+
+const startHeroPosition = { top: 16 , left: 2};
+
+board.createHero({ top: startHeroPosition.top * TILE_SIZE, left: startHeroPosition.left * TILE_SIZE});
+
+const fPositions = {
+    f0: { top: 17 , left: 2 },
+    f1: { top: 2 , left: 3 },
+    f2: { top: 2 , left: 8 },
+    f3: { top: 2 , left: 16 },
+}
+
+board.createElement({ item: 'forniture' , top: fPositions.f0.top * TILE_SIZE , left: fPositions.f0.left * TILE_SIZE});
+board.createElement({ item: 'forniture' , top: fPositions.f1.top * TILE_SIZE , left: fPositions.f1.left * TILE_SIZE});
+board.createElement({ item: 'forniture' , top: fPositions.f2.top * TILE_SIZE , left: fPositions.f2.left * TILE_SIZE});
+board.createElement({ item: 'forniture' , top: fPositions.f3.top * TILE_SIZE , left: fPositions.f3.left * TILE_SIZE});
+
+
+let occupiedPositions = [
+    startHeroPosition,
+    fPositions.f0,
+    fPositions.f1,
+    fPositions.f2,
+    fPositions.f3
+]
+
+
+function randowStartPosition() {
+
+    function validadePosition(top, left, positions) {
+        return true;
+    }
+
+    let top  = 0;
+    let left = 0;
+    let validatedPosition = false;
+
+    while (top < 2 || top > 17 || left < 1 || left > 18 || validatedPosition === false) {
+        top  = Math.floor(Math.random() * 17);
+        left = Math.floor(Math.random() * 18);
+
+        validatedPosition = validadePosition(top, left, occupiedPositions);
+    }
+    const randownPosition = { top: top , left: left };
+    console.log(randownPosition);
+    return randownPosition;
+}
+
+const chestPosition = randowStartPosition(occupiedPositions);
+
+board.createElement({item: 'chest' , top: chestPosition.top * TILE_SIZE, left: chestPosition.left * TILE_SIZE});
+
+
+// Demons:
+function createAllDemons(amountOfDemons) {
+    let i = 0;
+    while (i < amountOfDemons) {
+
+        const demonPosition = randowStartPosition(occupiedPositions);
+        occupiedPositions.push(demonPosition);
+        board.createDemon({ top: demonPosition.top * TILE_SIZE, left: demonPosition.left * TILE_SIZE});
+        i = i + 1;
+    }
+}
+
+createAllDemons(10);
+
+// Traps:
+function createAllTraps(amountOfTraps) {
+    let i = 0;
+    while (i < amountOfTraps) {
+
+        const trapPosition = randowStartPosition(occupiedPositions);
+        occupiedPositions.push(trapPosition);
+        board.createElement({ item: 'trap' , top: trapPosition.top * TILE_SIZE, left: trapPosition.left * TILE_SIZE});
+        i = i + 1;
+    }
+}
+
+createAllTraps(20);
+
